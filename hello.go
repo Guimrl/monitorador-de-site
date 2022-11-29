ecc4e0d0f54ddfe6bbc9cfa8c88367c5fb90d32e
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-11-29 20:46:08 -0300

    monitorando site

--- hello.go
+++ hello.go
@@ -9,27 +9,30 @@ import (
 func main() {
 
 	introducao()
-	exibeMenu()
-	comando := leComando()
 
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo logs...")
-	case 0:
-		fmt.Println("Saindo...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+	for {
+		exibeMenu()
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 
 }
 
 func introducao() {
 	nome := "Humano"
-	versao := 1.1
+	versao := 1.2
 	fmt.Println("Olá,", nome)
 	fmt.Println("Este programa está na versão", versao)
 }
@@ -51,7 +54,14 @@ func leComando() int {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://www.alura.com.br"
+	site := "http://random-status-code.herokuapp.com/"
 	resp, _ := http.Get(site)
-	fmt.Println(resp)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Staus Code:",
+			resp.StatusCode)
+	}
+
 }
