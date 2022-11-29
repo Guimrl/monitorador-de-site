fb17e53eb03bc54d917febd4b98bffa994ce2973
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-11-29 17:16:57 -0300

    first commit

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var nome string = "Guilherme"
+	var idade = 21
+	versao := 1.1
+	fmt.Println("Olá,", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+}
